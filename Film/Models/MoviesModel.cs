ba9a07f6251657080eb8c55521f7b3dba637f70a
MbahJoey <39154494+MbahJoey@users.noreply.github.com>
2018-06-07 23:13:12 +0300

    Controllers 2.0

--- Film/Models/MoviesModel.cs
+++ Film/Models/MoviesModel.cs
@@ -9,7 +9,6 @@ namespace Film.Models
 {
     public class MoviesModel
     {
-        #region Properties
             [Required(ErrorMessage = "Movie Title must contain from 3 to 50 symbols")]
             [StringLength(50, MinimumLength = 3)]
             public string Title { get; set; }
@@ -21,15 +20,5 @@ namespace Film.Models
             [Required(ErrorMessage = "Genre must contain from 3 to 30 symbols")]
             [StringLength(30, MinimumLength = 3)]
             public string GenreName { get; set; }
-        #endregion
-
-        #region Constructors
-        public MoviesModel(Movie e)
-        {
-            Title = e.Title;
-            GenreName = e.GenreName;
-            DirectorName = e.DirectorName;
-        }
-        #endregion
     }
 }
